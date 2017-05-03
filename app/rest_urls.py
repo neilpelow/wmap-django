495581f5163c83f0ee8f767f7aa5118a20a3b763
neilpelow <neilpelow@gmail.com>
2017-05-03 15:53:49 +0100

    register user

--- app/rest_urls.py
+++ app/rest_urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^user/(?P<email>[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)/$', rest_views.UserOther_R.as_view(), name='user-email'),
     url(r'^user/(?P<uid>\d+)/$', rest_views.UserOther_R.as_view(), name='user-username'),
     url(r'^updateposition/$', rest_views.UpdatePosition.as_view(), name='update-position'),
+    url(r'^signup/$', rest_views.register, name='signup'),
 ]
